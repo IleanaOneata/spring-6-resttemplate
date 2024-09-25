17a0a49dedd41914233305ec991aadd6fa989a7e
Ileana Oneata <ileana.oneata@yahoo.com>
2024-09-25 20:37:51 +0300

    Get List as JSON String

--- src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
@@ -1,16 +1,23 @@
 package guru.springframework.spring6resttemplate.client;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author ileanaoneata on 25.09.2024
  */
+
+@SpringBootTest
 class BeerClientImplTest {
+
+    @Autowired
+    BeerClientImpl beerClient;
 
     @Test
     void listBeers() {
-
+        beerClient.listBeers();
     }
 }
